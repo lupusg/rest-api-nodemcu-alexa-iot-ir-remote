a479dec88b3f5cbed3073af495994a326e58ed56
Vlad <just_vlad@yahoo.com>
2022-10-01 09:30:11 +0000

    Add user model

---
+++ src/api/models/user.js
@@ -0,0 +1,37 @@
+/**
+ * NodeMCU Alexa IOT Infrared Remote
+ *
+ * Convert old devices that are not compatible
+ * with Alexa into compatible ones using infrared
+ * signals.
+ *
+ * Rest API
+ *
+ * @desc Mongodb schema for "user" (authentification) collection.
+ * @since September 30, 2022
+ * @author Vlad-Marian Lupu
+ */
+
+'use strict';
+
+import mongoose from 'mongoose';
+
+export const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'user',
+    required: true,
+  },
+});
+
+export const User = mongoose.model('user', userSchema);
